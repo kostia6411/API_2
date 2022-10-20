5dffc0856f06fa3022f52950079d993bcfe4f85e
kostia6411 <91974940+kostia6411@users.noreply.github.com>
2022-10-20 19:59:02 +0300

    fixed_errors

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 
 import requests
 from terminaltables import AsciiTable
+from dotenv import load_dotenv
 
 
 def search_job_hh(language):
@@ -9,37 +10,43 @@ def search_job_hh(language):
     pages = 2
     vacancies_processed = 0
     average_salary = 0
+    location = 1
     salaries = []
     while page < pages:
         payload = {
-            "area": 1,
+            "area": location,
             "text": f"програмист {language}",
             "page": page
         }
         response = requests.get('https://api.hh.ru/vacancies', params=payload)
         response.raise_for_status()
-        pages = response.json()['pages']
+        response_payload = response.json()
+        pages = response_payload['pages']
         page += 1
-        for vacancy in response.json()["items"]:
-            if vacancy["salary"]:
-                predicted_salary = predict_rub_salary(vacancy["salary"])
-                if predicted_salary:
-                    salaries.append(predicted_salary)
-                    vacancies_processed += 1
+        for vacancy in response_payload["items"]:
+            if not vacancy["salary"]:
+                continue
+            predicted_salary = predict_rub_salary(
+                vacancy["salary"]["from"],
+                vacancy["salary"]["to"],
+                vacancy["salary"]["currency"]
+            )
+            if predicted_salary:
+                salaries.append(predicted_salary)
+                vacancies_processed += 1
     if vacancies_processed:
         average_salary = sum(salaries)/len(salaries)
     return{
-        "vacancies_found": response.json()['found'],
+        "vacancies_found": response_payload['found'],
         "vacancies_processed": vacancies_processed,
         "average_salary": int(average_salary)
     }
 
 
-def search_job_superjob(language):
+def search_job_superjob(language, superjob_key):
     salaries = []
     vacancies_processed = 0
     average_salary = 0
-    superjob_key = os.environ["SJ_KEY"]
     headers = {
         'X-Api-App-Id': superjob_key,
     }
@@ -49,38 +56,34 @@ def search_job_superjob(language):
     }
     response = requests.get('https://api.superjob.ru/2.0/vacancies/', headers=headers, params=payload)
     response.raise_for_status()
-    for vacancy in response.json()["objects"]:
-                predicted_salary = predict_rub_salary_for_superjob(vacancy)
-                if predicted_salary:
-                    salaries.append(predicted_salary)
-                    vacancies_processed += 1
+    response_payload = response.json()
+    for vacancy in response_payload["objects"]:
+        predicted_salary = predict_rub_salary(
+            vacancy["payment_from"],
+            vacancy["payment_to"],
+            vacancy["currency"]
+        )
+        if predicted_salary:
+            salaries.append(predicted_salary)
+            vacancies_processed += 1
     if vacancies_processed:
         average_salary = sum(salaries)/len(salaries)
     return{
-        "vacancies_found": response.json()['total'],
+        "vacancies_found": response_payload['total'],
         "vacancies_processed": vacancies_processed,
         "average_salary": int(average_salary)
     }
 
 
-def predict_rub_salary_for_superjob(salary):
-    if salary["currency"] == "rub":
-        if salary["payment_from"] and salary["payment_to"]:
-            return (salary["payment_from"] + salary["payment_to"]) / 2
-        elif salary["payment_from"]:
-            return salary["payment_from"] * 1.2
-        elif salary["payment_to"]:
-            return salary["payment_to"] * 0.8
-
-
-def predict_rub_salary(salary):
-    if salary["currency"] == "RUR":
-        if salary["from"] and salary["to"]:
-            return (salary["from"] + salary["to"]) / 2
-        elif salary["from"]:
-            return salary["from"] * 1.2
-        elif salary["to"]:
-            return salary["to"] * 0.8
+def predict_rub_salary(payment_from, payment_to, currency):
+    if not currency == "RUR" and not currency == "rub":
+        return
+    if payment_from and payment_to:
+        return (payment_from + payment_to) / 2
+    elif payment_from:
+        return payment_from * 1.2
+    elif payment_to:
+        return payment_to * 0.8
 
 
 def create_table(language_information):
@@ -92,12 +95,14 @@ def create_table(language_information):
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    superjob_key = os.environ["SJ_KEY"]
     programming_languages = ["JavaScript", "Java", "Python", "Ruby", "PHP", "C++", "CSS", "C#", "C", "Go" ]
     language_information_hh = {}
     language_information_superjob = {}
     for language in programming_languages:
         language_information_hh[language]=search_job_hh(language)
-        language_information_superjob[language]=search_job_superjob(language)
+        language_information_superjob[language]=search_job_superjob(language, superjob_key)
     finished_table_hh = AsciiTable(create_table(language_information_hh), "API_hh")
     finished_table_superjob = AsciiTable(create_table(language_information_superjob), "API_superjob")
     print(finished_table_hh.table)
